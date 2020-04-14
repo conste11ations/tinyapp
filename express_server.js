484e31930df289b77c1a3697e9f53cb536aef5ae
Rachel Kat <rachelkat@gmail.com>
2020-04-14 00:58:57 +0000

    add form for urls/new and added body-parser to parser buffer/info from POST method on form submission

--- express_server.js
+++ express_server.js
@@ -1,8 +1,10 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
 const PORT = 8080; // default port 8080
 
 app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({extended: true}));
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -22,11 +24,23 @@ app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
 
+//order matters! below must be defined before :shortURL otherwise 
+//Express will think 'new' is a route param
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+})
+
 app.get("/urls/:shortURL", (req, res) => {
   let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
   res.render("urls_show", templateVars);
 });
 
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // Log the POST request body to the console
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+});
+
+//Other routes
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
@@ -43,3 +57,20 @@ app.get("/fetch", (req, res) => {
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
+
+// functions
+function generateRandomString(input) {
+  // Importing 'crypto' module 
+  const crypto = require('crypto'), 
+
+  // Returns the names of supported hash algorithms  
+  // such as SHA1,MD5 
+  hash = crypto.getHashes(); 
+
+  // 'digest' is the output of hash function containing  
+  // only hexadecimal digits 
+  hashPwd = crypto.createHash('SHA1').update(input).digest('hex'); 
+  // truncate to only 6 alphanumeric string per instructions
+  return hashPwd.slice(0, 6);
+
+}
