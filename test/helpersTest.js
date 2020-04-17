cd792a08bece4f86b80b7b3d060778e26fa46eed
Rachel Kat <rachelkat@gmail.com>
2020-04-17 01:19:43 +0000

    wrapping up writing tests with assert/expect/should

--- test/helpersTest.js
+++ test/helpersTest.js
@@ -1,24 +1,46 @@
-const { assert } = require('chai');
+const { assert, expect, should } = require('chai');
+should();
 
 const { existingEmailChecker, getUserByEmail, urlsForUser, generateRandomString } = require('../helpers');
 
 const testUsers = {
   "userRandomID": {
-    id: "userRandomID", 
-    email: "user@example.com", 
+    id: "userRandomID",
+    email: "user@example.com",
     password: "purple-monkey-dinosaur"
   },
   "user2RandomID": {
-    id: "user2RandomID", 
-    email: "user2@example.com", 
+    id: "user2RandomID",
+    email: "user2@example.com",
     password: "dishwasher-funk"
   }
 };
 
-describe('getUserByEmail', function() {
-  it('should return a user with valid email', function() {
-    const user = getUserByEmail("user@example.com", users)
+const testUrlDatabase = {
+  "a123": { longURL: "http://www.lighthouselabs.ca", userID: "test1" },
+  "b246": { longURL: "http://www.google.com", userID: "test2" },
+  "c369": { longURL: "http://www.stackoverflow.com", userID: "test1" }
+};
+
+describe('#getUserByEmail', function () {
+  it('should return the right user given a valid email', function () {
+    const user = getUserByEmail("user@example.com", testUsers)
     const expectedOutput = "userRandomID";
-    // Write your assert statement here
+    expect(user).to.deep.equal(testUsers["userRandomID"]);
+    //    expect(user).to.have.property('email').equal("user@example.com");
+  });
+  it('should return a user object', () => {
+    const user = getUserByEmail("user@example.com", testUsers)
+    assert.typeOf(user, 'object');
   });
 });
+
+describe('#urlsForUser', function () {
+  it('should return the right url object(s) given a user id', function () {
+
+    const urls = urlsForUser("test1", testUrlDatabase)
+    expect(urls).to.have.property('a123');
+    urls.should.have.property('c369');
+
+  });
+})
